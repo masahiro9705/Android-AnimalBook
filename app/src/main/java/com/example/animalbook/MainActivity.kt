3d115f3712c02151914dae13e0f2ee21decdf4cb
masahiro9705 <masahiro970513@gmail.com>
2019-05-20 14:23:56 +0900

    完成

--- app/src/main/java/com/example/animalbook/MainActivity.kt
+++ app/src/main/java/com/example/animalbook/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.animalbook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,5 +12,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val fragment = titleFragment as? TitleFragment
         fragment?.setTitle("フラグメント動物図鑑")
+
+        nextButton.setOnClickListener {
+            val intent = Intent(
+                this,
+                SubActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
